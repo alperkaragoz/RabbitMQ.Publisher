279a6814438df90c598da7580643f5753179eca2
Alper Karagöz <alperkaragoz@outlook.com>
2023-07-09 23:59:42 +0300

    with correlationid

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
 // Bağlantıyı oluşturuyoruz.
@@ -8,28 +9,50 @@ factory.Uri = new("amqps://gagxjjis:g1rs6slF9bx-4tKOXHlP-NVypI8LlG8u@toad.rmq.cl
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-string queueName = "work-queue-example";
-
+string queueName = "request-response-queue-example";
 channel.QueueDeclare(
     queue: queueName,
     durable: false,
     exclusive: false,
     autoDelete: false);
 
-IBasicProperties basicProperties = channel.CreateBasicProperties();
-basicProperties.Persistent = true;
+// Consumer'dan dönecek olan sonucu elde edeceğimiz kuyruğun adını tanımlıyoruz.
+string replyQueueName = channel.QueueDeclare().QueueName;
+
+// Response sürecinde hangi request'e karşılık response'un yapılacağını ifade edecek olan korelasyonel değeri oluşturuyoruz.
+string correlationId = Guid.NewGuid().ToString();
+
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.CorrelationId = correlationId;
+// Response yapılacak queue "ReplyTo" property'sine atanıyor.
+properties.ReplyTo = replyQueueName;
 
 for (int i = 0; i < 100; i++)
 {
     await Task.Delay(200);
-
-    byte[] body = Encoding.UTF8.GetBytes("Hello " + i);
+    byte[] body = Encoding.UTF8.GetBytes("Request Message " + i);
     channel.BasicPublish(
         exchange: string.Empty,
         routingKey: queueName,
         body: body,
-        basicProperties: basicProperties);
+        basicProperties: properties
+        );
 
 }
+
+
+//Response kuyruğunu dinleme
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    if (e.BasicProperties.CorrelationId == correlationId)
+        Console.WriteLine($"Response = {Encoding.UTF8.GetString(e.Body.Span)}");
+};
 
 Console.Read();
