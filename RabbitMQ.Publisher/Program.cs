bd70eed30eac644573530d33c3fae578000d6f9d
Alper Karagöz <alperkaragoz@outlook.com>
2023-07-09 19:25:09 +0300

    basic exam

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -9,20 +9,31 @@ using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
 channel.ExchangeDeclare(
-    exchange: "topic-exchange-example",
-    type: ExchangeType.Topic);
+    exchange: "header-exchange-example",
+    type: ExchangeType.Headers
+    );
 
 for (int i = 0; i < 100; i++)
 {
     await Task.Delay(200);
     byte[] message = Encoding.UTF8.GetBytes($" Hello {i}");
-    Console.WriteLine("Write to sent Topic format: ");
-    string topic = Console.ReadLine();
-    channel.BasicPublish(
-        exchange: "topic-exchange-example",
-        routingKey: topic,
-        body: message);
+    Console.WriteLine("Please set header value: ");
+    string value = Console.ReadLine();
 
+    IBasicProperties basicProperties = channel.CreateBasicProperties();
+    basicProperties.Headers = new Dictionary<string, object>()
+    {
+        ["no"] = value
+    };
+
+
+    //basicProperties amacı, publish sürecinde "header-exchange-example" 'ın headerına bir değer vermek için kullanıyoruz.
+    channel.BasicPublish(
+        exchange: "header-exchange-example",
+        routingKey: string.Empty,
+        body: message,
+        basicProperties: basicProperties
+        );
 }
 
 Console.Read();
